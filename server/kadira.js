cc106cb7cdf5a7d43d70f59a6c6fb72e350a65c5
eferwe <eferwe@yahoo.com>
2015-10-01 16:29:46 +0200

    add kadira production debugger + css fix -remove user join template

---
+++ server/kadira.js
@@ -0,0 +1,6 @@
+Meteor.startup(function() {
+Kadira.connect('8fPGMHEzEDgKxXi6K', 'd216eff1-5074-4f98-9605-7d42b3f24944');
+});
+// export KADIRA_APP_ID=8fPGMHEzEDgKxXi6K
+
+// export KADIRA_APP_SECRET=d216eff1-5074-4f98-9605-7d42b3f24944
